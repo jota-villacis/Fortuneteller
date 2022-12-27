0df1bed19a655ab6284c6e36f7f628112d4546fc
Jimmy Villacis <g.villacis.jimmy@gmail.com>
2022-12-27 21:50:44 +1100

    security changes

--- fortuneteller/settings.py
+++ fortuneteller/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+#settings.py
+from dotenv import load_dotenv # type: ignore
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j5__z35q8g^o17dogthy7m!)%r!)$k@mupu*!(^w^%e+af*+d!'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
